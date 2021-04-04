6803e946513a50e892df861b9d9aa09522aa1a69
Elko <elkovandongen@gmail.com>
2021-04-04 22:06:10 +0200

    Fixed nav error

--- components/Header.js
+++ components/Header.js
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import {useState, useEffect} from 'react';
 import gsap from 'gsap'
 import { useRouter } from 'next/router'
+import { openMenu, closeMenu } from "./animations/menuAnimations";
 
 let tl = gsap.timeline()
 
@@ -11,110 +12,27 @@ const Header = ({dimensions}) => {
     const router = useRouter()
 
       useEffect(() => {
-        
-        const handleRouteChange = (url) => {
+        const handleRouteChange = (url, { shallow }) => {
           setMenuState({menuOpened:false})
         }
+    
         router.events.on('routeChangeStart', handleRouteChange)
-        
+    
         if (menuState.menuOpened == true) {
-            //Run opened menu animation
-            tl.to("nav", 0, {
-                css: { display: "block" }
-              })
-                .to("body", 0, { css: { overflow: "hidden" } })
-                .to(".App", 1, {
-                  y: dimensions.width <= 654 ? "70vh" : window.innerHeight / 1.5,
-                  ease: "expo.inOut"
-                })
-                .to(".hamburger-menu span", 0.6, {
-                  delay: -1,
-                  scaleX: 0,
-                  transformOrigin: "50% 0%",
-                  ease: "expo.inOut"
-                })
-                .to("#Path_1", 0.4, {
-                  delay: -0.6,
-                  css: {
-                    strokeDashoffset: 10,
-                    strokeDasharray: 5
-                  }
-                })
-                .to("#Path_2", 0.4, {
-                  delay: -0.6,
-                  css: {
-                    strokeDashoffset: 10,
-                    strokeDasharray: 20
-                  }
-                })
-                .to("#Line_1", 0.4, {
-                  delay: -0.6,
-                  css: {
-                    strokeDashoffset: 40,
-                    strokeDasharray: 18
-                  }
-                })
-                .to("#circle", 0.6, {
-                  delay: -0.8,
-                  css: {
-                    strokeDashoffset: 0
-                  },
-                  ease: "expo.inOut"
-                })
-                .to(".hamburger-menu-close", 0.6, {
-                  delay: -0.8,
-                  css: { display: "block" }
-                });
+          //Run opened menu animation
+          openMenu(dimensions.width);
+      }else{
+          //run close menu animation
+          closeMenu();
+      }
 
-        }else{
-            //run close menu animation
-            tl.to(".App", 1, {
-                y: 0,
-                ease: "expo.inOut"
-              })
-                .to("#circle", 0.6, {
-                  delay: -0.6,
-                  css: {
-                    strokeDashoffset: -193,
-                    strokeDasharray: 227
-                  }
-                })
-                .to("#Path_1", 0.4, {
-                  delay: -0.6,
-                  css: {
-                    strokeDashoffset: 10,
-                    strokeDasharray: 10
-                  }
-                })
-                .to("#Path_2", 0.4, {
-                  delay: -0.6,
-                  css: {
-                    strokeDashoffset: 10,
-                    strokeDasharray: 10
-                  }
-                })
-                .to("#Line_1", 0.4, {
-                  delay: -0.6,
-                  css: {
-                    strokeDashoffset: 40,
-                    strokeDasharray: 40
-                  }
-                })
-                .to(".hamburger-menu span", 0.6, {
-                  delay: -0.6,
-                  scaleX: 1,
-                  transformOrigin: "50% 0%",
-                  ease: "expo.inOut"
-                })
-                .to(".hamburger-menu-close", 0, {
-                  delay: -0.1,
-                  css: { display: "none" }
-                })
-                .to("body", 0, { css: { overflow: "auto" } })
-                .to("nav", 0, {
-                  css: { display: "none" }
-                });
+        return () => {
+          router.events.off('routeChangeStart', handleRouteChange)
         }
+        
+        
+        
+        
     });
 
     return (
@@ -125,7 +43,7 @@ const Header = ({dimensions}) => {
                         <Link href="/">i.CoN</Link>
                     </div>
                     <div className="nav-toggle">
-                        <div onClick = {()=> setMenuState({menuOpened:true})}className="hamburger-menu">
+                        <div onClick = {()=> setMenuState({menuOpened:true})} className="hamburger-menu">
                             <span></span>
                             <span></span>
                         </div>
@@ -145,7 +63,7 @@ const Header = ({dimensions}) => {
                     </div>
                 </div>
             </div>
-
+          
         </div>
     );
 };
